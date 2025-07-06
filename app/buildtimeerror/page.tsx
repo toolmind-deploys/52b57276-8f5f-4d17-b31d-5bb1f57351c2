612a7579ccd8ca56626557ca585dbea6fc1a8d9a
toolmind-deploys <toolminddeploys@gmail.com>
2025-07-06 15:52:01 -0700

    chore: update app/buildtimeerror/page.tsx

--- app/buildtimeerror/page.tsx
+++ app/buildtimeerror/page.tsx
@@ -1,11 +1,9 @@
-export default function BuildTimeErrorPage() {
-  // TypeScript build time error by referencing a non-existing variable
-  const result: string = nonExistentVariable;
+import { foo } from '@/lib/nonexistent'
 
-  return (
-    <div>
-      <h1>This page triggers a build time error.</h1>
-      <p>Value of result: {result}</p>
-    </div>
-  );
+export default function BuildTimeErrorPage() {
+    return (
+        <div>
+            <h1>This page triggers a build time error.</h1>
+        </div>
+    )
 }
